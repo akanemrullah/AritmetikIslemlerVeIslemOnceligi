ecaa03e4d273d95b9fb7e89d06a18c6cfcb993d5
Emrullah AKAN <akanemrullah@gmail.com>
2024-02-21 05:27:08 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // Gerekli degiskenler tanimlandi.
         int a, b, c, sonuc;
 
+        // Scanner nesnesi projemize dahil edildi.
         Scanner input = new Scanner(System.in);
 
+        // Kullanicidan girdiler alinip bu veriler degiskenlere atandi.
         System.out.print("A sayisini giriniz:");
         a = input.nextInt();
 
@@ -16,8 +19,10 @@ public class Main {
         System.out.print("C sayisini giriniz:");
         c = input.nextInt();
 
+        // Kullanici girdileri ile beraber islemler yapildi.
         sonuc = a + b * c - b;
 
+        // Islemin sonucu kullanici ile paylasildi.
         System.out.println(sonuc);
     }
 }
